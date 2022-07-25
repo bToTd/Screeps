513adeb3fcdbb8e3b96537a9cb2b89ceb20634a8
黄文川 <huangwenchuan@sandboxol.com>
2022-07-25 11:15:59 +0800

    init vscode

---
+++ cmd.js
@@ -0,0 +1,6 @@
+﻿Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'Upgrader1' );
+Game.creeps['Harvester1'].memory.role = 'harvester';
+Game.creeps['Upgrader1'].memory.role = 'upgrader';
+Game.spawns['Spawn1'].spawnCreep( [WORK,WORK,WORK,WORK,CARRY,MOVE,MOVE],
+    'HarvesterBig',
+    { memory: { role: 'harvester' } } );
